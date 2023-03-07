caa7aebd93b54af647d135a9fd156209291ab3a6
Terence Mbu <mbu.terence@gmail.com>
2023-03-07 13:20:28 +0100

    backend - tickethack v1

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("./models/connection")
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var tripsRouter = require('./routes/trips')
 
 var app = express();
 const cors = require('cors');
@@ -16,6 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/trips', tripsRouter)
 
 module.exports = app;
